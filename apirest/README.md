7c57a3270ce6ff9960b06fbd93a95af1a1ca7904
pcnandes <pcnandes@yahoo.com.br>
2019-10-31 00:29:56 -0300

    inicio

---
+++ apirest/README.md
@@ -0,0 +1,5 @@
+**https://www.youtube.com/watch?v=bpBRFNKg8k4**
+
+**Criacao do projeto**
+https://start.spring.io/
+
---
+++ apirest/src/main/java/com/produtos/apirest/ApirestApplication.java
@@ -0,0 +1,14 @@
+package com.produtos.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestApplication {
+	
+	// classe de inicialização do spring boot
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestApplication.class, args);
+	}
+
+}
